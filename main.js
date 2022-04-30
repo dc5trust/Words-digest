03a3f444fad4c0bb1395a00008442b5f2bec0521
Hector Bonilla <dc5trust@gmail.com>
2022-04-29 22:45:51 -0700

    added some basic styling and fetched the random words api

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log('hello World');
+const content = document.querySelector('.content-box');
+const newWord = document.querySelector('.word');
+const pronunciation = document.querySelector('.pronunciation')
+const definition = document.querySelector('.definition');
+
+async function generateRandomWords (){
+    const result = await fetch('https://random-words-api.vercel.app/word');
+    const data = await result.json();
+
+    content.innerText = data[0].pronunciation;
+    //data[0].word
+    //data[0].defintiion
+    //data[0].pronunciation
+    return console.log(data[0]);
+}
+
+generateRandomWords();
